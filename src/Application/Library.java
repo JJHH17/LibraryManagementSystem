6758c45960df4a4bafd1bd4de58596bda7a3d591
James Hatfield <james.hatfield17@icloud.com>
2025-07-27 11:53:17 +0100

    Added ArrayList in Library class which contains books. Added setter and getter methods for the arraylist contents

--- src/Application/Library.java
+++ src/Application/Library.java
@@ -1,13 +1,25 @@
 package Application;
 
+import java.util.ArrayList;
+
 public class Library {
     private String name;
+    private ArrayList<Book> books;
 
     public Library(String name) {
         this.name = name;
+        this.books = new ArrayList<>();
     }
 
     public String getName() {
         return name;
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public ArrayList<Book> getBooks() {
+        return books;
     }
 }
