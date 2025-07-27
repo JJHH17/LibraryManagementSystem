db3d7b50ee095d3ac1bdf1d6055149e9834bd148
James Hatfield <james.hatfield17@icloud.com>
2025-07-27 17:21:44 +0100

    Added method to create a book table in database. Resolved bug with authentication, due to incorrect credential names

--- src/Application/Database.java
+++ src/Application/Database.java
@@ -22,8 +22,28 @@ public class Database {
         }
 
         /** From the db.properties credentials file */
-        this.dbUrl = properties.getProperty("dbUrl");
-        this.dbUsername = properties.getProperty("dbUsername");
-        this.dbPassword = properties.getProperty("dbPassword");
+        this.dbUrl = properties.getProperty("db.url");
+        this.dbUsername = properties.getProperty("db.username");
+        this.dbPassword = properties.getProperty("db.password");
+    }
+
+    public void createBookTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS book (" +
+                "bookId SERIAL PRIMARY KEY," +
+                "name VARCHAR(255) NOT NULL UNIQUE," +
+                "author VARCHAR(255)," +
+                "numberOfRents INTEGER," +
+                "isAvailable BOOLEAN NOT NULL," +
+                "libraryName VARCHAR(255) NOT NULL)";
+
+        // Creating the connection to database
+        try (Connection connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
+            System.out.println("Table created successfully");
+        } catch (SQLException e) {
+            System.err.println("Failed to create table: " + e.getMessage());
+            throw new RuntimeException("Database operation failed (Table creation)", e);
+        }
     }
 }
