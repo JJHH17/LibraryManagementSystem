30502d000da06ef2cfb2ff4eb8552f3815c875b1
James Hatfield <james.hatfield17@icloud.com>
2025-07-29 18:48:40 +0100

    Added select options method and buttons, which will direct the user to seperate pages to complete actions

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -2,6 +2,7 @@ package Application;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
@@ -15,10 +16,23 @@ public class Main extends Application {
         primaryStage.show();
     }
 
+    /** Page designed to allow the user to select what they wish to do */
     public void selectOption(Stage primaryStage) {
-        Button button = new Button("Hello");
+        Label title = new Label("Library Management System");
         VBox layout = new VBox(10);
-        layout.getChildren().add(button);
+
+
+        // App navigation buttons
+        Button addBook = new Button("Add a Book");
+        Button editBook = new Button("Edit a Book");
+        Button deleteBook = new Button("Delete a Book");
+        Button deleteAllBooks = new Button("Delete all books");
+        Button getBooks = new Button("Get all books");
+
+
+        layout.getChildren().addAll(title, addBook, editBook, deleteBook, deleteAllBooks, getBooks);
+
+        // Adds and creates interface
         Scene scene = new Scene(layout, 300, 250);
         primaryStage.setScene(scene);
     }
