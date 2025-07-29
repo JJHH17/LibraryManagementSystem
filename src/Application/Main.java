9bfe1e763d882a95edbc36ac683a96b8e024a4b1
James Hatfield <james.hatfield17@icloud.com>
2025-07-29 18:34:01 +0100

    Added skeleton for Javafx application which now opens a window and appends a button

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,8 +1,29 @@
 package Application;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        selectOption(primaryStage);
+        primaryStage.setTitle("Library Management System");
+        primaryStage.show();
+    }
+
+    public void selectOption(Stage primaryStage) {
+        Button button = new Button("Hello");
+        VBox layout = new VBox(10);
+        layout.getChildren().add(button);
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+    }
+
     public static void main(String[] args) {
-        UserInterface userInterface = new UserInterface();
-        userInterface.start();
+        launch(args);
     }
 }
