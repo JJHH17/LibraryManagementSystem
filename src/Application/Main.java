b28da17cf2e1a6f2237f497ea560d19717d83079
James Hatfield <james.hatfield17@icloud.com>
2025-08-02 17:25:22 +0100

    Removed instance call for UserInterface class

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,8 +1,6 @@
 package Application;
 
 public class Main {
-    static UserInterface userInterface = new UserInterface();
-
     public static void main(String[] args) {
         UserInterface.launcher(args);
     }
